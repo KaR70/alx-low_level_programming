583c16d2afc413c83a5f78e5c6c2ceb984a597f3
KA70 <kareemosama409@gmail.com>
2023-08-23 02:20:12 +0300

    10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print alphabet x10, in lowercase followed by a new line
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int l = 'a';
+	int count = 1;
+
+	while (count <= 10)
+	{
+		print_alphabet();
+		count++;
+	}
