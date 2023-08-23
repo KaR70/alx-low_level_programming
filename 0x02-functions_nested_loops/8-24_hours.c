d366afc10e264ae9041438337d460c5ced70a2eb
KA70 <kareemosama409@gmail.com>
2023-08-23 10:52:05 +0300

    prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - Count from 00:00 to 23:59
+ *
+ * @void: This is the entry
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int a = 0;
+	int b = 0;
+
+	while (a <= 23)
+	{
+		while (b <= 59)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+			b++;
+		}
+		a++;
+	}
+}
+				
+
