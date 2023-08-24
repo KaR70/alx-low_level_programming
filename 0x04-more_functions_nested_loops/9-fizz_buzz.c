d8821f9b6cb106ad05261cd1c2d68c556036e00a
KA70 <kareemosama409@gmail.com>
2023-08-24 21:48:09 +0300

    prints a triangle

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Print multiples of three, five and both.
+ * Return: 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 ==0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+}
