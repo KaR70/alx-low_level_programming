65e6d29f594b06e0857c807200a9715b4ab97dde
KA70 <kareemosama409@gmail.com>
2023-08-24 22:28:18 +0300

    prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,7 +8,6 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
-	int count;
 	
 	if (size <= 0)
 		_putchar('\n');
@@ -16,12 +15,12 @@ void print_triangle(int size)
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j < count; j++)
+			for (j = 1; j <= size; j++)
 			{
 				if (j <= (size - i))
 					_putchar(32);
 				else
-					 _putchar('#');
+					_putchar('#');
 			}
 			_putchar('\n');
 		}
